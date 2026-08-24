e03174cf761a362a650129eccdd12d51451165aa
Abdulaziz Srour <abdulaziz.srour@its-digital.de>
2026-08-24 09:15:55 +0200

    first commit

---
+++ GeoRisk Backend/src/main/java/com/example/georisk/GeoRiskApplication.java
@@ -0,0 +1,13 @@
+package com.example.georisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoRiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoRiskApplication.class, args);
+	}
+
+}
---
+++ GeoRisk Backend/src/test/java/com/example/georisk/GeoRiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.georisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoRiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
